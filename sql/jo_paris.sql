68ccaf4371c83c6606d0e86faef04e0ac665a466
AkilalAmazigh <amazighakilal@gmail.com>
2022-10-31 21:54:25 +0100

    sql update with heritage

--- sql/jo_paris.sql
+++ sql/jo_paris.sql
@@ -2,207 +2,304 @@
 #        Script MySQL.
 #------------------------------------------------------------
 
+drop database if exists jo_paris; 
+create database jo_paris; 
+use jo_paris; 
 
 #------------------------------------------------------------
-# Table: Categorie
+# Table: user
 #------------------------------------------------------------
-
-CREATE TABLE Categorie(
-        idcategorie Int NOT NULL ,
-        libelle     Text NOT NULL
-	,CONSTRAINT Categorie_PK PRIMARY KEY (idcategorie)
-)ENGINE=InnoDB;
-
-
-#------------------------------------------------------------
-# Table: Client
-#------------------------------------------------------------
-
-CREATE TABLE Client(
-        idclient Int NOT NULL ,
-        nom      Varchar (50) NOT NULL ,
-        email    Varchar (50) NOT NULL ,
-        tel      Varchar (50) NOT NULL ,
-        mdp      Varchar (50) NOT NULL
-	,CONSTRAINT Client_PK PRIMARY KEY (idclient)
-)ENGINE=InnoDB;
-
-
-#------------------------------------------------------------
-# Table: Evenement
-#------------------------------------------------------------
-
-CREATE TABLE Evenement(
-        idevenement  Int NOT NULL ,
-        type         Varchar (50) NOT NULL ,
-        dateEvent    Date NOT NULL ,
-        nomEvenement Varchar (50) NOT NULL ,
-        description  Text NOT NULL ,
-        adresse      Varchar (50) NOT NULL ,
-        horraireD    Time NOT NULL ,
-        horraireF    Time NOT NULL ,
-        capacite     Int NOT NULL ,
-        idcategorie  Int NOT NULL
-	,CONSTRAINT Evenement_PK PRIMARY KEY (idevenement)
-
-	,CONSTRAINT Evenement_Categorie_FK FOREIGN KEY (idcategorie) REFERENCES Categorie(idcategorie)
-)ENGINE=InnoDB;
-
-
-#------------------------------------------------------------
-# Table: Media
-#------------------------------------------------------------
-
-CREATE TABLE Media(
-        idmedia Int NOT NULL ,
-        url     Varchar (100) NOT NULL
-	,CONSTRAINT Media_PK PRIMARY KEY (idmedia)
-)ENGINE=InnoDB;
-
-
-#------------------------------------------------------------
-# Table: Typeservice
-#------------------------------------------------------------
-
-CREATE TABLE Typeservice(
-        idtypeservices Int NOT NULL ,
-        libelle        Varchar (50) NOT NULL
-	,CONSTRAINT Typeservice_PK PRIMARY KEY (idtypeservices)
-)ENGINE=InnoDB;
-
-
-#------------------------------------------------------------
-# Table: Service
-#------------------------------------------------------------
-
-CREATE TABLE Service(
-        idservice      Int NOT NULL ,
-        libelle        Varchar (50) NOT NULL ,
-        adresse        Varchar (50) NOT NULL ,
-        prix           Float NOT NULL ,
-        tel            Varchar (50) NOT NULL ,
-        email          Varchar (50) NOT NULL ,
-        idtypeservices Int NOT NULL
-	,CONSTRAINT Service_PK PRIMARY KEY (idservice)
-
-	,CONSTRAINT Service_Typeservice_FK FOREIGN KEY (idtypeservices) REFERENCES Typeservice(idtypeservices)
-)ENGINE=InnoDB;
-
+create table user
+(
+    iduser int(3) NOT NULL auto_increment,
+    nom VARCHAR(50) NOT NULL ,
+    email varchar(255) NOT NULL ,
+    mdp varchar(255) NOT NULL ,
+    tel Varchar (50) NOT NULL ,
+    role enum("admin", "user"),
+    primary key(iduser)
+);
 
 #------------------------------------------------------------
 # Table: Client_Pro
 #------------------------------------------------------------
 
-CREATE TABLE Client_Pro(
-        idclient  Int NOT NULL ,
-        num_Siret Varchar (50) NOT NULL ,
-        adresse   Varchar (50) NOT NULL ,
-        nom       Varchar (50) NOT NULL ,
-        email     Varchar (50) NOT NULL ,
-        tel       Varchar (50) NOT NULL ,
-        mdp       Varchar (50) NOT NULL
-	,CONSTRAINT Client_Pro_PK PRIMARY KEY (idclient)
-
-	,CONSTRAINT Client_Pro_Client_FK FOREIGN KEY (idclient) REFERENCES Client(idclient)
-)ENGINE=InnoDB;
+CREATE TABLE Client_Pro
+(
+    iduser  Int NOT NULL ,
+    num_Siret Varchar (50) NOT NULL ,
+    adresse   Varchar (50) NOT NULL ,
+    PRIMARY KEY (iduser) ,
+    FOREIGN KEY (iduser) REFERENCES user(iduser)
+);
 
 
 #------------------------------------------------------------
 # Table: Client_Particulier
 #------------------------------------------------------------
 
-CREATE TABLE Client_Particulier(
-        idclient Int NOT NULL ,
-        prenom   Varchar (50) NOT NULL ,
-        nom      Varchar (50) NOT NULL ,
-        email    Varchar (50) NOT NULL ,
-        tel      Varchar (50) NOT NULL ,
-        mdp      Varchar (50) NOT NULL
-	,CONSTRAINT Client_Particulier_PK PRIMARY KEY (idclient)
+CREATE TABLE Client_Particulier
+(
+    iduser Int NOT NULL ,
+    prenom   Varchar (50) NOT NULL ,
+    PRIMARY KEY (iduser) ,
+    FOREIGN KEY (iduser) REFERENCES user(iduser)
+);
 
-	,CONSTRAINT Client_Particulier_Client_FK FOREIGN KEY (idclient) REFERENCES Client(idclient)
-)ENGINE=InnoDB;
+
+#------------------------------------------------------------
+# Table: Categorie
+#------------------------------------------------------------
+
+CREATE TABLE Categorie
+(
+    idcategorie Int NOT NULL auto_increment,
+    libelle     Text NOT NULL ,
+    PRIMARY KEY (idcategorie)
+);
+
+#------------------------------------------------------------
+# Table: Evenement
+#------------------------------------------------------------
+
+CREATE TABLE Evenement
+(
+    idevenement  Int NOT NULL auto_increment,
+    type         Varchar (50) NOT NULL ,
+    dateEvent    Date NOT NULL ,
+    nomEvenement Varchar (50) NOT NULL ,
+    description  Text NOT NULL ,
+    adresse      Varchar (50) NOT NULL ,
+    horraireD    Time NOT NULL ,
+    horraireF    Time NOT NULL ,
+    capacite     Int NOT NULL ,
+    idcategorie  Int NOT NULL ,
+    PRIMARY KEY (idevenement) ,
+    FOREIGN KEY (idcategorie) REFERENCES Categorie(idcategorie)
+);
+
+
+#------------------------------------------------------------
+# Table: Media
+#------------------------------------------------------------
+
+CREATE TABLE Media
+(
+    idmedia Int NOT NULL auto_increment,
+    url     Varchar (100) NOT NULL ,
+    PRIMARY KEY (idmedia)
+);
+
+
+#------------------------------------------------------------
+# Table: Typeservice
+#------------------------------------------------------------
+
+CREATE TABLE Typeservice
+(
+    idtypeservices Int NOT NULL auto_increment,
+    libelle        Varchar (50) NOT NULL,
+    PRIMARY KEY (idtypeservices)
+);
+
+
+#------------------------------------------------------------
+# Table: Service
+#------------------------------------------------------------
+
+CREATE TABLE Service
+(
+    idservice      Int NOT NULL auto_increment,
+    libelle        Varchar (50) NOT NULL ,
+    adresse        Varchar (50) NOT NULL ,
+    prix           Float NOT NULL ,
+    tel            Varchar (50) NOT NULL ,
+    email          Varchar (50) NOT NULL ,
+    idtypeservices Int NOT NULL,
+    PRIMARY KEY (idservice),
+    FOREIGN KEY (idtypeservices) REFERENCES Typeservice(idtypeservices)
+);
 
 
 #------------------------------------------------------------
 # Table: Pub
 #------------------------------------------------------------
 
-CREATE TABLE Pub(
-        idpub       Int  Auto_increment  NOT NULL ,
-        typePub     Varchar (50) NOT NULL ,
-        budget      Float NOT NULL ,
-        dateDebut   Date NOT NULL ,
-        dateFin     Date NOT NULL ,
-        idevenement Int NOT NULL ,
-        idclient    Int NOT NULL
-	,CONSTRAINT Pub_PK PRIMARY KEY (idpub)
-
-	,CONSTRAINT Pub_Evenement_FK FOREIGN KEY (idevenement) REFERENCES Evenement(idevenement)
-	,CONSTRAINT Pub_Client_Pro0_FK FOREIGN KEY (idclient) REFERENCES Client_Pro(idclient)
-)ENGINE=InnoDB;
+CREATE TABLE Pub
+(
+    idpub       Int NOT NULL auto_increment ,
+    typePub     Varchar (50) NOT NULL ,
+    budget      Float NOT NULL ,
+    dateDebut   Date NOT NULL ,
+    dateFin     Date NOT NULL ,
+    idevenement Int NOT NULL ,
+    iduser    Int NOT NULL , 
+    PRIMARY KEY (idpub) ,
+    FOREIGN KEY (idevenement) REFERENCES Evenement(idevenement) ,
+    FOREIGN KEY (iduser) REFERENCES user(iduser)
+);
 
 
 #------------------------------------------------------------
 # Table: Commenter
 #------------------------------------------------------------
 
-CREATE TABLE Commenter(
-        idclient    Int NOT NULL ,
-        idevenement Int NOT NULL ,
-        contenu     Text NOT NULL ,
-        note        Int NOT NULL ,
-        dateCom     Date NOT NULL
-	,CONSTRAINT Commenter_PK PRIMARY KEY (idclient,idevenement)
-
-	,CONSTRAINT Commenter_Client_FK FOREIGN KEY (idclient) REFERENCES Client(idclient)
-	,CONSTRAINT Commenter_Evenement0_FK FOREIGN KEY (idevenement) REFERENCES Evenement(idevenement)
-)ENGINE=InnoDB;
+CREATE TABLE Commenter
+(
+    iduser    Int NOT NULL auto_increment ,
+    idevenement Int NOT NULL ,
+    contenu     Text NOT NULL ,
+    note        Int NOT NULL ,
+    dateCom     Date NOT NULL ,   
+    PRIMARY KEY (iduser,idevenement) ,
+    FOREIGN KEY (iduser) REFERENCES user(iduser) ,
+    FOREIGN KEY (idevenement) REFERENCES Evenement(idevenement)
+);
 
 #------------------------------------------------------------
 # Table: Inscription
 #------------------------------------------------------------
 
-CREATE TABLE Inscription(
-        idclient    Int NOT NULL ,
-        idevenement Int NOT NULL ,
-        dateD       Date NOT NULL ,
-        commentaire Text NOT NULL ,
-        statut      Varchar (50) NOT NULL
-	,CONSTRAINT Inscription_PK PRIMARY KEY (idclient,idevenement)
-
-	,CONSTRAINT Inscription_Client_FK FOREIGN KEY (idclient) REFERENCES Client(idclient)
-	,CONSTRAINT Inscription_Evenement0_FK FOREIGN KEY (idevenement) REFERENCES Evenement(idevenement)
-)ENGINE=InnoDB;
+CREATE TABLE Inscription
+(
+    iduser    Int NOT NULL auto_increment ,
+    idevenement Int NOT NULL ,
+    dateD       Date NOT NULL ,
+    commentaire Text NOT NULL ,
+    statut      Varchar (50) NOT NULL ,
+    PRIMARY KEY (iduser,idevenement) ,
+    FOREIGN KEY (iduser) REFERENCES user(iduser) ,
+    FOREIGN KEY (idevenement) REFERENCES Evenement(idevenement)
+);
 
 
 #------------------------------------------------------------
 # Table: Louer
 #------------------------------------------------------------
 
-CREATE TABLE Louer(
-        idclient  Int NOT NULL ,
-        idservice Int NOT NULL ,
-        heureD TIME,
-        heureF TIME,
-        ,CONSTRAINT Louer_PK PRIMARY KEY (idclient, idservice)
-)ENGINE=InnoDB;
-
-
-create table user (
-    iduser int(3) not null auto_increment,
-    nom VARCHAR(50),
-    prenom VARCHAR(50),
-    email varchar(255),
-    mdp varchar(255),
-    rolee varchar(20),
-    primary key(iduser)
+CREATE TABLE Louer
+(
+    iduser  Int NOT NULL auto_increment ,
+    idservice Int NOT NULL ,
+    heureD TIME,
+    heureF TIME,
+    PRIMARY KEY (iduser, idservice)
 );
 
+#------------------------------------------------------------
+# Procedure stockee pour l_insertion d_un Client_Particulier
+#------------------------------------------------------------
+
+delimiter $
+create procedure insertClientPar (IN c_nom varchar(50), IN c_email varchar(50), IN c_mdp varchar(50), 
+IN c_tel varchar(50), IN c_role varchar(50), IN c_prenom varchar(50)) 
+Begin 
+        Declare c_iduser int(3); 
+        
+        insert into user values (null, c_nom, c_email, c_mdp, c_tel, c_role ); 
+        select iduser into c_iduser 
+        from user 
+        where nom = c_nom and email =c_email and mdp=c_mdp and tel= c_tel; 
+        insert into Client_Particulier values (c_iduser, c_prenom );
+End $
+delimiter  ;
+
+#------------------------------------------------------------
+# Procedure stockee pour l_insertion d_un Client_Pro
+#------------------------------------------------------------
+
+delimiter $
+create procedure insertClientPro (IN c_nom varchar(50), IN c_email varchar(50), IN c_mdp varchar(50), 
+IN c_tel varchar(50), IN c_role varchar(50), IN c_num_Siret varchar(50), IN c_adresse varchar(50) ) 
+Begin 
+        Declare c_iduser int(3); 
+        
+        insert into user values (null, c_nom, c_email, c_mdp, c_tel, c_role ); 
+        select iduser into c_iduser 
+        from user 
+        where nom = c_nom and email =c_email and mdp=c_mdp and tel= c_tel; 
+        insert into Client_Pro values (c_iduser, c_num_Siret, c_adresse);
+End $
+delimiter  ;
+
+#------------------------------------------------------------
+# Procedure stockee pour la supression d_un Client_Partivulier
+#------------------------------------------------------------
+
+delimiter $
+create procedure deleteClientPar (IN c_iduser int(3) ) 
+Begin 
+        delete from Client_Particulier where iduser = c_iduser ;     
+        delete from user where iduser = c_iduser ;   
+End $
+delimiter  ; 
+
+#------------------------------------------------------------
+# Procedure stockee pour la supression d_un Client_Pro
+#------------------------------------------------------------
+
+delimiter $
+create procedure deleteClientPro (IN c_iduser int(3) ) 
+Begin 
+        delete from Client_Pro where iduser = c_iduser ;     
+        delete from user where iduser = c_iduser ;   
+End $
+delimiter  ; 
+
+#------------------------------------------------------------
+# Procedure stockee pour la modification d_un Client_Particulier
+#------------------------------------------------------------
+
+delimiter $
+create procedure updateClientPar (IN c_nom varchar(50), IN c_email varchar(50), 
+	IN c_mdp varchar(50), IN c_tel varchar(50) , IN c_role varchar(50), 
+	IN c_prenom varchar(50), IN c_iduser int(3)) 
+Begin  
+        update user set nom = c_nom, email = c_email, mdp = c_mdp, tel = c_tel, role =c_role 
+        where iduser = c_iduser ; 
+
+        update Client_Particulier set prenom = c_prenom where iduser = c_iduser ;
+End $
+delimiter  ; 
+
+#------------------------------------------------------------
+# Procedure stockee pour la modification d_un Client_Pro
+#------------------------------------------------------------
+
+delimiter $
+create procedure updateClientPro (IN c_nom varchar(50), IN c_email varchar(50), 
+	IN c_mdp varchar(50), IN c_tel varchar(50) , IN c_role varchar(50), 
+	IN c_num_Siret varchar(50), IN c_adresse varchar(50), IN c_iduser int(3)) 
+Begin  
+        update user set nom = c_nom, email = c_email, mdp = c_mdp, tel = c_tel, role =c_role 
+        where iduser = c_iduser ; 
+
+        update Client_Pro set num_Siret = c_num_Siret, adresse = c_adresse  where iduser = c_iduser ;
+End $
+delimiter  ; 
+
+#---------------------------------------------------------
+# insertion de USER dans la table user
+#--------------------------------------------------------
+
+insert into user values (NULL, "Akilal", "a@gmail.com", "123", "0605743353", "admin");
+
+#----------------------------------------------------------------
+# insertion de Client_Particulier dans la table Client_Particulier
+#-----------------------------------------------------------------
+
+call insertClientPar ("Levy", "l@gmail.com", "123", "0754862541", "user", "Dan");
+
+#---------------------------------------------------------
+# insertion de Client_Pro dans la table Client_Pro
+#--------------------------------------------------------
+
+call insertClientPro ("Housset", "h@gmail.com", "123", "0875463251", "user", "84351248751428", "12 rue de Cléry Paris");
 
 #---------------------------------------------------------
 # insertion de Categories dans la table Categorie
 #--------------------------------------------------------
+
 insert into Categorie values (NULL, "Epreuves");
 insert into Categorie values (NULL, "Ceremonies");
 
@@ -210,6 +307,7 @@ insert into Categorie values (NULL, "Ceremonies");
 #---------------------------------------------------------
 # insertion de Typeservice dans la table Typeservice
 #--------------------------------------------------------
+
 insert into Typeservice values (NULL, "Hotels");
 insert into Typeservice values (NULL, "Bars & Restaurants");
 insert into Typeservice values (NULL, "Culture");
@@ -217,11 +315,10 @@ insert into Typeservice values (NULL, "Sport");
 
 
 #---------------------------------------------------------
-# insertion de `Service` dans la table `Service`
+# insertion de Service dans la table Service
 #--------------------------------------------------------
 
-
-insert into `Service` values 
+insert into Service values  
         (NULL, "Novotel Paris Vaugirart", "257 Rue de Vaugirard", 200, "0155689745", "NovotelVaugirard@gmail.com", 01),
         (NULL, "Hotel Molitor", "13 Rue Nungesser et Coli", 500, "0158451275", "HotelMolitor@gmail.com", 01),
         (NULL, "Hotel F1 Paris St Ouen", "29 Ruz Docteur Babinski ", 43, "0181247956", "HotelF1@gmail.com", 01),
@@ -239,17 +336,12 @@ insert into `Service` values
 # insertion de Evenement dans la table Evenement
 #--------------------------------------------------------
 
-insert into `Evenement` values (NULL, "Epreuve natation", "2024-07-14","Epreuve natation masculine", "Piscine ouverte", "Centre Aquatique de Saint-Denis", "10:30", "12:00", 4000, 01);
-insert into `Evenement` values (NULL, "Epreuve natation", "2024-07-15","Epreuve natation feminine", "Piscine ouverte", "Centre Aquatique de Saint-Denis", "10:30", "12:00", 4000, 01);
-insert into `Evenement` values (NULL, "Epreuve course", "2024-07-16","Epreuve course feminine", "Stade ouvert", "Stade de Saint-Denis", "10:30", "12:00", 1600, 01);
-insert into `Evenement` values (NULL, "Epreuve course", "2024-07-17","Epreuve course masculine", "Stade ouvert", "Stade de Saint-Denis", "10:30", "12:00", 2300, 01);
-insert into `Evenement` values (NULL, "Epreuve Lancer de Disque", "2024-07-18","Epreuve force feminine", "Stade ouvert", "Stade de Saint-Denis", "10:30", "12:00", 3655, 01);
-insert into `Evenement` values (NULL, "Epreuve Lancer de Disque", "2024-07-19","Epreuve force masculine", "Stade ouvert", "Stade de Saint-Denis", "10:30", "12:00", 5000, 01);
+insert into Evenement values 
+        (NULL, "Epreuve natation", "2024-07-14","Epreuve natation masculine", "Piscine ouverte", "Centre Aquatique de Saint-Denis", "10:30", "12:00", 4000, 01),
+        (NULL, "Epreuve natation", "2024-07-15","Epreuve natation feminine", "Piscine ouverte", "Centre Aquatique de Saint-Denis", "10:30", "12:00", 4000, 01),
+        (NULL, "Epreuve course", "2024-07-16","Epreuve course feminine", "Stade ouvert", "Stade de Saint-Denis", "10:30", "12:00", 1600, 01),
+        (NULL, "Epreuve course", "2024-07-17","Epreuve course masculine", "Stade ouvert", "Stade de Saint-Denis", "10:30", "12:00", 2300, 01),
+        (NULL, "Epreuve Lancer de Disque", "2024-07-18","Epreuve force feminine", "Stade ouvert", "Stade de Saint-Denis", "10:30", "12:00", 3655, 01),
+        (NULL, "Epreuve Lancer de Disque", "2024-07-19","Epreuve force masculine", "Stade ouvert", "Stade de Saint-Denis", "10:30", "12:00", 5000, 01);
 
 
-#---------------------------------------------------------
-# insertion de USER dans la user Categorie
-#--------------------------------------------------------
-insert into user values (NULL, "Levy", "Dan", "d@gmail.com", "123", "admin");
-insert into user values (NULL, "Akilal", "Amazigh", "m@gmail.com", "123", "admin");
-insert into user values (NULL, "Housse", "Thomas", "t@gmail.com", "123", "admin");
